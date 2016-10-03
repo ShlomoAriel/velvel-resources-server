820be6d45b9111bf73dab2255899d0e46e7713e5
Shlomo Ariel <shlomoa@balink.net>
2016-10-03 12:13:25 +0300

    make it better2

--- server.js
+++ server.js
@@ -708,7 +708,7 @@ function findLike(model, searchString) {
             }
         });
 }
-app.listen(3001);
+app.listen(process.env.PORT || 3001);
 console.log('Listening on localhost 3001');
 mongoose.connect('mongodb://user1:a1345678@ds015995.mlab.com:15995/velevltest');
 // mongoose.connect('mongodb://localhost/example');
