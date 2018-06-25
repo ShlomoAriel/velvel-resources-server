e387d8eccdbe58c1a1e32051b1b3de61ef75fa0d
Shlomo Ariel <shlomoariel@gmail.com>
2018-06-25 06:15:21 +0300

    d

--- models/dailyWorker.js
+++ models/dailyWorker.js
@@ -37,6 +37,11 @@ var DailyWorkerSchema = new Schema({
         required: true,
         unique: false 
     },
+    restDay: { 
+        type: Boolean,
+        required: true,
+        default: false
+    },
     hours: { 
         type: Number,
         required: true,
