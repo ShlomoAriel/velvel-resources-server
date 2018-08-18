92f793cf9ef6010979f131f58594a9732a4ee5f7
Shlomo Ariel <shlomoariel@gmail.com>
2018-08-18 08:35:54 +0300

    testing

--- server.js
+++ server.js
@@ -217,6 +217,8 @@ app.post('/api/addUserToSite/', passport.authenticate('jwt', { session: false })
                         }
                         res.status(200).send('OK');
                     });
+                } else{
+                    res.status(200).send('OK');
                 }
             }
         });
